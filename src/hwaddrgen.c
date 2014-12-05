8a5549f02871b2d0646c4ac9d938f67af60c3a99
root <ewalbridge@gmail.com>
2014-12-05 16:47:50 -0500

    changed to hwaddrgen.c

--- src/hwaddrgen.c
+++ src/hwaddrgen.c
@@ -22,37 +22,67 @@
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	SOFTWARE.
 
- */
+	*/
+
+//
+//format mac address *
+//type: any or local
+//prefix
+//case
+//count
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
+#include <getopt.h>
 
-#define MAC_GLB_LEN 16
-#define MAC_LOC_LEN 4
-
-int main(void)
+int main(int argc, char* argv[])
 {
 	struct timeval tm;
 	gettimeofday(&tm, NULL);
 	srand(tm.tv_sec + tm.tv_usec * 1000000ul);
 
-	static char mac_glb_hex[MAC_GLB_LEN] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-	static char mac_loc_hex[MAC_LOC_LEN] = { '2', '6', 'A', 'E' };
+	enum { hwaddr_len = 12 };
+	enum { hwaddr_glb_len = 16 };
+	enum { hwaddr_loc_len = 4 };
+	static char hwaddr_glb[hwaddr_glb_len] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	static char hwaddr_loc[hwaddr_loc_len] = { '2', '6', 'A', 'E' };
 
 	int i;
-	for (i = 0; i < 12; i++)
+	size_t space = 1;
+	char* mac = malloc(space);
+
+	int local = 0;
+	for (i = 0; i < hwaddr_len; i++)
 	{
-		if (i == 1)
+		if (i == 1 && local == 1)
 		{
-			printf("%c", mac_loc_hex[rand() % MAC_LOC_LEN]);
+			mac[i] = hwaddr_loc[rand() % hwaddr_loc_len];
 		}
 		else
 		{
-			printf("%c", mac_glb_hex[rand() % MAC_GLB_LEN]);
+			mac[i] = hwaddr_glb[rand() % hwaddr_glb_len];
 		}
+		space++;
+		mac = realloc(mac, space);
 	}
 
-	printf("\n");
+	char* mac_f1 = "%c%c%c%c%c%c%c%c%c%c%c%c\n";
+	char* mac_f2 = "%c%c-%c%c-%c%c-%c%c-%c%c-%c%c\n";
+	char* mac_f3 = "%c%c:%c%c:%c%c:%c%c:%c%c:%c%c\n";
+	char* mac_f4 = "%c%c%c%c.%c%c%c%c.%c%c%c%c\n";
+
+	printf(mac_f1, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], mac[6], mac[7], mac[8], mac[9], mac[10], mac[11]);
+	printf(mac_f2, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], mac[6], mac[7], mac[8], mac[9], mac[10], mac[11]);
+	printf(mac_f3, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], mac[6], mac[7], mac[8], mac[9], mac[10], mac[11]);
+	printf(mac_f4, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], mac[6], mac[7], mac[8], mac[9], mac[10], mac[11]);
+
+	//for (i = 0; i < hwaddr_len; i++)
+	//{
+	//	printf("%c", mac[i]);
+	//}
+	//printf("\n");
+
+	free(mac);
 	return 0;
 }
